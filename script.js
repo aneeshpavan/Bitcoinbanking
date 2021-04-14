73c27cb5c26985d6f12082f16ccb0cd6ee28c70f
Aneesh Pavan Prodduturu <43541169+aneeshpavan@users.noreply.github.com>
2021-04-14 13:06:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText);
+
+function sendMoney(){
+   var enterName = document.getElementById("enterName").value;
+   var enterAmount = parseInt(document.getElementById("enterAmount").value);
+
+   if (enterAmount > 500000) {
+      alert("Insufficient Balance.")
+   } else {
+      var findUserBankAccount = enterName + "BitcoinBalance";
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+      document.getElementById("myAccountBalance").innerText = myAccountBalance
+      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+      alert(`Transaction Successful !!  
+      ${enterAmount} ₿ sent to ${enterName}`)
+
+      // transaction history 
+      var createPTag = document.createElement("li");
+      var textNode = document.createTextNode(`${enterAmount} ₿ Transferred Successfully to ${enterName} on ${Date()}.`);
+      createPTag.appendChild(textNode);
+      var element = document.getElementById("transaction-history-body");
+      element.insertBefore(createPTag, element.firstChild);
+   }
+}
+
+function donate(){
+   var person = alert("Thank you for your kind gesture. You are a true treasure to me :)");
+}
+
+
+
